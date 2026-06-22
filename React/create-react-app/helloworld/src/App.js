7cb14818142965eccc553baec6cf6f0223ef0e59
tharunpatnayak <vankudoththarunpatnayak9390@gmail.com>
2026-06-22 15:06:01 +0530

    creating react app

---
+++ React/create-react-app/helloworld/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello world</h1>
+    </div>
+  );
+}
+function Tha(){
+  return(
+    <div className="Tha">
+    
+      CVR
+    </div>
+  )
+}
+
+export default App;
+export {Tha}
